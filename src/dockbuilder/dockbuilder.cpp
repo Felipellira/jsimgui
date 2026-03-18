96ca2e600da3390f035a561fc690dcbb9a950e15
Felipellira <felipeluigilira@proton.me>
2026-03-18 17:47:56 -0300

    Add printf debug logging to all DockBuilder C++ functions

--- src/dockbuilder/dockbuilder.cpp
+++ src/dockbuilder/dockbuilder.cpp
@@ -2,31 +2,45 @@
 
 #include <imgui.h>
 #include <imgui_internal.h>
+#include <cstdio>
 
 // clang-format off
 static auto const DOCKBUILDER = bindings([]() {
 
 bind_fn("DockBuilder_DockWindow", [](std::string window_name, unsigned int node_id) {
+    printf("[DockBuilder C++] DockWindow('%s', %u)\n", window_name.c_str(), node_id);
     ImGui::DockBuilderDockWindow(window_name.c_str(), node_id);
+    printf("[DockBuilder C++] DockWindow done\n");
 });
 
 bind_fn("DockBuilder_AddNode", [](unsigned int node_id, int flags) {
-    return ImGui::DockBuilderAddNode(node_id, static_cast<ImGuiDockNodeFlags>(flags));
+    printf("[DockBuilder C++] AddNode(%u, %d)\n", node_id, flags);
+    auto result = ImGui::DockBuilderAddNode(node_id, static_cast<ImGuiDockNodeFlags>(flags));
+    printf("[DockBuilder C++] AddNode returned %u\n", result);
+    return result;
 });
 
 bind_fn("DockBuilder_RemoveNode", [](unsigned int node_id) {
+    printf("[DockBuilder C++] RemoveNode(%u)\n", node_id);
     ImGui::DockBuilderRemoveNode(node_id);
+    printf("[DockBuilder C++] RemoveNode done\n");
 });
 
 bind_fn("DockBuilder_RemoveNodeChildNodes", [](unsigned int node_id) {
+    printf("[DockBuilder C++] RemoveNodeChildNodes(%u)\n", node_id);
     ImGui::DockBuilderRemoveNodeChildNodes(node_id);
+    printf("[DockBuilder C++] RemoveNodeChildNodes done\n");
 });
 
 bind_fn("DockBuilder_SetNodeSize", [](unsigned int node_id, float w, float h) {
-    // Access the node directly to avoid ImVec2 ABI issues in extern C
+    printf("[DockBuilder C++] SetNodeSize(%u, %f, %f)\n", node_id, w, h);
     ImGuiDockNode* node = ImGui::DockBuilderGetNode(node_id);
+    printf("[DockBuilder C++] GetNode returned %p\n", (void*)node);
     if (node) {
         node->SizeRef = ImVec2(w, h);
+        printf("[DockBuilder C++] SizeRef set\n");
+    } else {
+        printf("[DockBuilder C++] WARNING: node is null!\n");
     }
 });
 
@@ -35,6 +49,7 @@ bind_fn("DockBuilder_SplitNode", [](
     int split_dir,
     float size_ratio_for_node_at_dir
 ) {
+    printf("[DockBuilder C++] SplitNode(%u, dir=%d, ratio=%f)\n", node_id, split_dir, size_ratio_for_node_at_dir);
     ImGuiID out_id_at_dir = 0;
     ImGuiID out_id_at_opposite_dir = 0;
 
@@ -46,6 +61,8 @@ bind_fn("DockBuilder_SplitNode", [](
         &out_id_at_opposite_dir
     );
 
+    printf("[DockBuilder C++] SplitNode => atDir=%u, atOpposite=%u\n", out_id_at_dir, out_id_at_opposite_dir);
+
     auto result = js_val::object();
     result.set("atDir", out_id_at_dir);
     result.set("atOppositeDir", out_id_at_opposite_dir);
@@ -53,7 +70,9 @@ bind_fn("DockBuilder_SplitNode", [](
 });
 
 bind_fn("DockBuilder_Finish", [](unsigned int node_id) {
+    printf("[DockBuilder C++] Finish(%u)\n", node_id);
     ImGui::DockBuilderFinish(node_id);
+    printf("[DockBuilder C++] Finish done\n");
 });
 
 });
